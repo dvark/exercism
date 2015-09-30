22a5f4a4835ccf7065fb94b41b1fabc7e65a289c
junedev <catherinejune86@gmail.com>
2015-10-01 00:51:28 +0200

    commiting exercises

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,16 +1,15 @@
-module.exports = function Gigasecond(input_date){
+function Gigasecond(input_date){
   this.input_milliseconds = input_date.getTime();
-  this.date = date;
 }
 
-function date(){
-  var result_milliseconds = this.input_milliseconds + (1e9 *1000);
-  var date = new Date();
-  date.setTime(result_milliseconds);
+Gigasecond.prototype.date = function(){
+  var result_milliseconds = this.input_milliseconds + 1e12;
+  var date = new Date(result_milliseconds);
 
   // Why do I have to make the result less exact to pass the tests?
   date.setHours(0,0,0,0);
-    
+
   return date;
 }
 
+module.exports = Gigasecond;
