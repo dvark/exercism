3ba0063d563b8e5952bfbd09e1e0fa8e3fe8ef3e
junedev <catherinejune86@gmail.com>
2016-08-29 19:55:33 +0200

    iterating on leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,10 +1,3 @@
 module.exports = function isLeapYear(year){
-  var result = false;
-  if(year%4===0){
-    result = true;
-  }
-  if(year%100===0 && year%400!==0){
-    result = false;
-  }
-  return result;
+    return year%4 === 0 && (year%100 !== 0 || year%400 === 0);
 }
