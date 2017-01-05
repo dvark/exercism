d8f21572b01813b9edaaf99decfe01d9e5b1b50d
junedev <catherinejune86@gmail.com>
2017-01-05 21:17:07 +0100

    minor adaptions

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,27 +1,29 @@
 function Series(inputString) {
-    if(isNaN(inputString)) {
+    if (isNaN(inputString)) {
         throw new Error('Invalid input.');
     }
     this.numberArray = inputString.split('').map((n) => parseInt(n));
 }
 
-Series.prototype.largestProduct = function(digits) {
-    if(digits < 0) {
+Series.prototype.largestProduct = function (digits) {
+    if (digits < 0) {
         throw new Error('Invalid input.');
     }
-    if(this.numberArray.length < digits) {
+    if (this.numberArray.length < digits) {
         throw new Error('Slice size is too big.');
     }
 
     var maxProduct = 0;
-    for(var i = 0; i <= this.numberArray.length - digits; i++) {
+    for (var i = 0; i <= this.numberArray.length - digits; i++) {
         var slice = this.numberArray.slice(i, i + digits);
-        currentProduct = slice.reduce((product, cur) => {return product * cur;},1)
-        if (currentProduct > maxProduct) {
-            maxProduct = currentProduct;
-        }
+        currentProduct = slice.reduce(multiply, 1)
+        maxProduct = Math.max(currentProduct, maxProduct);
     }
     return maxProduct;
+}
+
+function multiply(a, b) {
+    return a * b;
 }
 
 module.exports = Series;
