1bb8110d032154de83ab09faa8c0f773335084fe
junedev <catherinejune86@gmail.com>
2017-01-05 21:12:33 +0100

    adapting nit pick comment

--- javascript/bracket-push/bracket-push.js
+++ javascript/bracket-push/bracket-push.js
@@ -3,15 +3,11 @@ const matches = ['{}', '()', '[]'];
 module.exports = function bracket(inputString) {
     let stack = [];
     inputString.split('').forEach((e) => {
-        if(matches.includes(last(stack) + e)) {
-            stack.pop();
+        if (matches.includes(stack[0] + e)) {
+            stack.shift();
         } else {
-            stack.push(e);
+            stack.unshift(e);
         }
     })
     return stack.length === 0;
-}
-
-function last(array) {
-    return array[array.length - 1];
 }
