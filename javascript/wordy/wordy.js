d3479127bc615182d6e508df91a1eb0fd22e2295
junedev <catherinejune86@gmail.com>
2017-01-15 18:15:49 +0100

    iterate on wordy

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,20 +1,27 @@
 var regex = /(-?\d+)\s(.+?)\s(-?\d+)(.*)$/;
 
-function WordProblem(question){
+var map = {
+    'plus': (a, b) => a + b,
+    'minus': (a, b) => a - b,
+    'multiplied by': (a, b) => a * b,
+    'divided by': (a, b) => a / b
+}
+
+function WordProblem(question) {
     this.question = question;
 }
 
-WordProblem.prototype.answer = function(){
+WordProblem.prototype.answer = function () {
     var result = null;
     do {
         var matches = regex.exec(this.question);
-        if(matches) {
-            result = calculate(matches.slice(1,4));
+        if (matches) {
+            result = calculate(matches.slice(1, 4));
             this.question = result + matches[4];
         }
     } while (regex.test(this.question));
 
-    if(result === null) {
+    if (result === null) {
         throw new ArgumentError();
     }
     return result
@@ -23,25 +30,10 @@ WordProblem.prototype.answer = function(){
 function calculate(arguments) {
     var number1 = parseInt(arguments[0]);
     var number2 = parseInt(arguments[2]);
-    var result;
-    switch (arguments[1]) {
-        case 'plus':
-            result = number1 + number2;
-            break;
-        case 'minus':
-            result = number1 - number2;
-            break;
-        case 'multiplied by':
-            result = number1 * number2;
-            break;
-        case 'divided by':
-            result = number1 / number2;
-            break;
-    }
-    return result;
+    return map[arguments[1]](number1, number2);
 }
 
-function ArgumentError(){}
+function ArgumentError() { }
 ArgumentError.prototype = Error.prototype;
 
 module.exports = {
